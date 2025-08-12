8ae1893d9c49f9434f3761cdbceb7fdcd5c1c8c4
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-12 19:57:54 -0300

    Fix middleware order by moving router setup above body parsing middleware

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ import path from "path";
 const app = express();
 
 app.use(cors())
+app.use("/", router);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/", router);
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
